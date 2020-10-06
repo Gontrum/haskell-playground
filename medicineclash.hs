3dfd8889970d4aeea7ff2f790411e3bae714b9ec
David <Gontrum@users.noreply.github.com>
2020-10-06 05:29:12 +0000

    - removed commented code

--- medicineclash.hs
+++ medicineclash.hs
@@ -18,23 +18,18 @@ addPrescription :: Medicine -> Prescription -> Medicine
 addPrescription (Medicine name oldPrescriptions) prescription = Medicine name (prescription : oldPrescriptions)
 getNames :: [Medicine] -> [Name]
 getNames = map (\(Medicine name _) -> name)
+mergeMedicineWithGivenNames :: [Medicine] -> [Name] -> [Medicine]
+mergeMedicineWithGivenNames medicines names = map (\name -> (mergeToSingleMedicine (filterByName name medicines))) names
+  where   filterByName :: Name -> [Medicine] -> [Medicine]
+          filterByName name medicines = filter (\(Medicine medName _) -> name == medName) medicines
+mergeToSingleMedicine :: [Medicine] -> Medicine
+mergeToSingleMedicine (firstMed:medicines) = foldl mergeTwoMedicines firstMed medicines
+mergeTwoMedicines :: Medicine -> Medicine -> Medicine
+mergeTwoMedicines (Medicine name prescriptions) (Medicine _ prescriptions2) = Medicine name $ prescriptions ++ prescriptions2
 
 data Patient = Patient [Medicine]
 addMedicine :: Patient -> Medicine -> Patient
 addMedicine (Patient oldMedicine) medicine = Patient $ medicine : oldMedicine
-
-allMedicinesAreTakenOn :: DispenseDate -> [Medicine] -> [Name] -> Bool
-allMedicinesAreTakenOn day meds names = names `subset` namesOfMedicinesTakenOn day
-  where     namesOfMedicinesTakenOn day = getNames $ (medicinesTakenOn day) meds
-
-medicinesTakenOn :: DispenseDate -> [Medicine] -> [Medicine]
-medicinesTakenOn day = filter (\med -> day `isInDispenseTime` med)
-
-isInDispenseTime :: DispenseDate -> Medicine -> Bool
-isInDispenseTime day (Medicine _ prescriptions) = all condition prescriptions
-    where   condition presciption = day <= lastDay presciption && day >= (firstDay presciption)
-            firstDay (Prescription fd _) = fd
-            lastDay (Prescription fd days) = addDays days fd
 
 daysWhenMedicineWasTaken :: Medicine -> [DispenseDate]
 daysWhenMedicineWasTaken (Medicine _ presciptions) = removeDuplicates $ concat $ map daysPerPrescription presciptions
@@ -45,33 +40,14 @@ daysPerPrescription (Prescription date numberOfDays) = date:(daysPerPrescription
 removeDuplicates :: Eq a => [a] -> [a]
 removeDuplicates = foldl (\acc curr -> if curr `elem` acc then acc else curr : acc) []
 
-subset :: (Foldable t, Eq a) => [a] -> t a -> Bool
-subset a b = null [x | x <- a, (x `elem` b) == False]
-
-clashx :: Patient -> [Name] -> [DispenseDate]
-clashx (Patient meds) names = meds
-    & map getDays
-    & concat
-    & filter (\day -> allMedicinesAreTakenOn day meds names)
-    & removeDuplicates
-
 clash :: Patient -> [Name] -> [DispenseDate]
-clash (Patient meds) names = foldl reducer (daysWhenMedicineWasTaken (firstMedicine filteredMedicine)) filteredMedicine
-    where reducer :: [DispenseDate] -> Medicine -> [DispenseDate]
-          reducer acc med = intersect (daysWhenMedicineWasTaken med) acc
-          filteredMedicine :: [Medicine]
-          filteredMedicine = mergeNamedMedicines meds names
+clash (Patient meds) names = foldl intersectAll daysOfFirstMedicine filteredMedicine
+    where intersectAll :: [DispenseDate] -> Medicine -> [DispenseDate]
+          intersectAll acc med = intersect acc $ daysWhenMedicineWasTaken med
+          filteredMedicine = mergeMedicineWithGivenNames meds names
+          daysOfFirstMedicine = daysWhenMedicineWasTaken $ firstMedicine filteredMedicine
           firstMedicine :: [Medicine] -> Medicine
           firstMedicine (first:_) = first
-
-mergeNamedMedicines :: [Medicine] -> [Name] -> [Medicine]
-mergeNamedMedicines medicines names = map (\name -> (mergeToSingleMedicine (filterByName name medicines))) names
-  where   filterByName :: Name -> [Medicine] -> [Medicine]
-          filterByName name medicines = filter (\(Medicine medName _) -> name == medName) medicines
-mergeToSingleMedicine :: [Medicine] -> Medicine
-mergeToSingleMedicine (firstMed:medicines) = foldl mergeTwoMedicines firstMed medicines
-mergeTwoMedicines :: Medicine -> Medicine -> Medicine
-mergeTwoMedicines (Medicine name prescriptions) (Medicine _ prescriptions2) = Medicine name $ prescriptions ++ prescriptions2
 
 -- Tests
 main :: IO ()
@@ -92,113 +68,6 @@ main = hspec $ do
       length (daysWhenMedicineWasTaken (Medicine "penicillin" [(Prescription (fromGregorian 2020 1 1) 20), (Prescription (fromGregorian 2020 12 1) 20)])) `shouldBe` 40
     it "Medicin with two prescriptions with 20 days each but with overlapping of 10 days should contain 30 days"  $ do
       length (daysWhenMedicineWasTaken (Medicine "penicillin" [(Prescription (fromGregorian 2020 1 1) 20), (Prescription (fromGregorian 2020 1 11) 20)])) `shouldBe` 30
-
-  describe "isInDispenseTime" $ do 
-      it "day inside dispense time should result in true" $ do
-        isInDispenseTime (fromGregorian 2020 1 15) (Medicine "penicillin" [Prescription (fromGregorian 2020 1 1) 30]) `shouldBe` True
-      it "first day of dispense time should result in true" $ do
-        isInDispenseTime (fromGregorian 2020 1 1) (Medicine "penicillin" [Prescription (fromGregorian 2020 1 1) 30]) `shouldBe` True
-      it "last day of dispense time should result in true" $ do
-        isInDispenseTime (fromGregorian 2020 2 1) (Medicine "penicillin" [Prescription (fromGregorian 2020 1 1) 31]) `shouldBe` True
-      it "day after last day of dispense time should result in false" $ do
-        isInDispenseTime (fromGregorian 2020 2 1) (Medicine "penicillin" [Prescription (fromGregorian 2020 1 1) 30]) `shouldBe` False
-      it "day before first day of dispense time should result in false" $ do
-        isInDispenseTime (fromGregorian 2019 12 31) (Medicine "penicillin" [Prescription (fromGregorian 2020 1 1) 30]) `shouldBe` False
-      it "day far away of dispense time should result in false" $ do
-        isInDispenseTime (fromGregorian 1945 5 8) (Medicine "penicillin" [Prescription (fromGregorian 2020 1 1) 30]) `shouldBe` False
-
-  describe "allMedicinesAreTakenOn" $ do
-    it "should return false when only some of the given names were taken on a given day" $ do
-      let medicine1 = penicillin prescription_January_First
-          medicine2 = aspirin prescription_January_First
-          medicineList = [medicine1, medicine2]
-          nameList = ["penicillin", "sleep"]
-      allMedicinesAreTakenOn (fromGregorian 2020 1 1) medicineList nameList `shouldBe` False
-    it "should return false when the medicines were found but taken on different days" $ do
-      let medicine1 = penicillin prescription_January_First
-          medicine2 = aspirin prescription_February_Second
-          medicineList = [medicine1, medicine2]
-          nameList = ["penicillin", "aspirin"]
-      allMedicinesAreTakenOn (fromGregorian 2020 1 1) medicineList nameList `shouldBe` False
-    it "should return true when the medicines 'penicillin' and 'aspirin' were found and taken on the same day" $ do
-      let medicine1 = penicillin prescription_January_First
-          medicine2 = aspirin prescription_January_First
-          medicineList = [medicine1, medicine2]
-          nameList = ["penicillin", "aspirin"]
-      allMedicinesAreTakenOn (fromGregorian 2020 1 1) medicineList nameList `shouldBe` True
-    it "should return true when the medicines 'penicillin' and 'sleep' were found and taken on the same day" $ do
-      let medicine1 = penicillin prescription_January_First
-          medicine2 = Medicine "sleep" [prescription_January_First]
-          medicineList = [medicine1, medicine2]
-          nameList = ["penicillin", "sleep"]
-      allMedicinesAreTakenOn (fromGregorian 2020 1 1) medicineList nameList `shouldBe` True
-    it "should return true when the medicines 'penicillin' and 'brandNewMedicine' were found and taken on the same day" $ do
-      let medicine1 = penicillin prescription_January_First
-          medicine2 = Medicine "brandNewMedicine" [prescription_January_First]
-          medicineList = [medicine1, medicine2]
-          nameList = ["penicillin", "brandNewMedicine"]
-      allMedicinesAreTakenOn (fromGregorian 2020 1 1) medicineList nameList `shouldBe` True
-    it "should return true when the medicines 'penicillin' and 'aspirin', but also 'brandNewMedicine' were found and taken on the same day" $ do
-      let medicine1 = penicillin prescription_January_First
-          medicine2 = aspirin prescription_January_First
-          medicine3 = Medicine "brandNewMedicine" [prescription_January_First]
-          medicineList = [medicine1, medicine2, medicine3]
-          nameList = ["penicillin", "brandNewMedicine"]
-      allMedicinesAreTakenOn (fromGregorian 2020 1 1) medicineList nameList `shouldBe` True
-    it "should return true when the medicines 'penicillin' and 'brandNewMedicine', but also 'aspirin' were found and taken on the same day" $ do
-      let medicine1 = penicillin prescription_January_First
-          medicine2 = Medicine "brandNewMedicine" [prescription_January_First]
-          medicine3 = aspirin prescription_January_First
-          medicineList = [medicine1, medicine2, medicine3]
-          nameList = ["penicillin", "brandNewMedicine"]
-      allMedicinesAreTakenOn (fromGregorian 2020 1 1) medicineList nameList `shouldBe` True
-    it "should return true when the medicines 'penicillin' and 'Placebo', but also 'aspirin' were found and taken on the same day" $ do
-      let medicine1 = penicillin prescription_January_First
-          medicine2 = Medicine "Placebo" [prescription_January_First]
-          medicine3 = aspirin prescription_January_First
-          medicineList = [medicine1, medicine2, medicine3]
-          nameList = ["penicillin", "Placebo"]
-      allMedicinesAreTakenOn (fromGregorian 2020 1 1) medicineList nameList `shouldBe` True
-    it "should return true when the medicines 'penicillin' and 'aspirin' were found and taken on the same day (2.2.2020)" $ do
-      let medicine1 = penicillin prescription_February_Second
-          medicine2 = aspirin prescription_February_Second
-          medicineList = [medicine1, medicine2]
-          nameList = ["penicillin", "aspirin"]
-      allMedicinesAreTakenOn (fromGregorian 2020 2 2) medicineList nameList `shouldBe` True
-    it "should return true when the medicines 'penicillin' and 'aspirin' were found and taken on the same day (3.3.2020)" $ do
-      let medicine1 = penicillin prescription_March_Third
-          medicine2 = aspirin prescription_March_Third
-          medicineList = [medicine1, medicine2]
-          nameList = ["penicillin", "aspirin"]
-      allMedicinesAreTakenOn (fromGregorian 2020 3 3) medicineList nameList `shouldBe` True
-    it "should return false when the medicines 'penicillin' and 'brandNewMedicine' and 'aspirin' were found, but 'aspirin' was taken on a different day" $ do
-      let medicine1 = penicillin prescription_January_First
-          medicine2 = Medicine "brandNewMedicine" [prescription_January_First]
-          medicine3 = aspirin prescription_March_Third
-          medicineList = [medicine1, medicine2, medicine3]
-          nameList = ["penicillin", "brandNewMedicine", "aspirin"]
-      allMedicinesAreTakenOn (fromGregorian 2020 1 1) medicineList nameList `shouldBe` False
-    it "should return true when the medicines 'penicillin' and 'brandNewMedicine' and 'aspirin' were found, and 'aspirin' taken on another day, too" $ do
-      let medicine1 = penicillin prescription_January_First
-          medicine2 = Medicine "brandNewMedicine" [prescription_January_First]
-          medicine_asp_day1 = aspirin prescription_January_First
-          medicine_asp_day2 = aspirin (Prescription (fromGregorian 2020 1 2) 1)
-          medicineList = [medicine1, medicine2, medicine_asp_day1, medicine_asp_day2]
-          nameList = ["penicillin", "brandNewMedicine", "aspirin"]
-      allMedicinesAreTakenOn (fromGregorian 2020 1 1) medicineList nameList `shouldBe` True
-    it "should return true when the medicines 'penicillin' and 'brandNewMedicine' and 'aspirin' were found, and 'aspirin' taken on 02.01.2020, too" $ do
-      let medicine1 = penicillin prescription_January_First
-          medicine2 = Medicine "brandNewMedicine" [prescription_January_First]
-          medicine_asp_day1 = aspirin prescription_January_First
-          medicine_asp_day2 = aspirin prescription_February_Second
-          medicineList = [medicine1, medicine2, medicine_asp_day1, medicine_asp_day2]
-          nameList = ["penicillin", "brandNewMedicine", "aspirin"]
-      allMedicinesAreTakenOn (fromGregorian 2020 1 1) medicineList nameList `shouldBe` True
-    it "should return false when a medicine with multiple dates in list was not taken on the specified date" $ do
-      let penicillin1 = penicillin prescription_January_First
-      let penicillin2 = penicillin prescription_February_Second
-      let penicillin3 = penicillin prescription_March_Third
-      allMedicinesAreTakenOn (fromGregorian 2020 4 4) [penicillin1, penicillin2, penicillin3] ["penicillin"] `shouldBe` False
 
   describe "medicine clash" $ do
     it "should return empty list for not clashing medicines" $ do
@@ -229,11 +98,3 @@ main = hspec $ do
           medicine2_day2 = aspirin (Prescription (fromGregorian 2020 2 2) 1)
           patient = Patient [medicine1_day1, medicine2_day2]
       clash patient ["aspirin", "penicillin"] `shouldBe` [fromGregorian 2020 2 2]
-
--- allMedicinesAreTakenOn :: Day -> [Medicine] -> [Name] -> Bool
--- allMedicinesAreTakenOn day meds names = meds
---     & filter (\med -> day `elem` getDays med)
---     & map getName
---     & intersect names
---     & sort
---     & (==) (sort names)
